52ad82e1c188c4dac3bd2235a04e09cdac6be978
Christian Lauke <christian.lauke@web.de>
2018-10-14 18:23:36 +0200

    GCE - GKE Deployment Test v2

---
+++ deploy.sh
@@ -0,0 +1,22 @@
+# Building Images
+docker build -t clauke/k8s-multi-docker-client:latest -t clauke/k8s-multi-docker-client:$SHA ./client/Dockerfile ./client
+docker build -t clauke/k8s-multi-docker-server:latest -t clauke/k8s-multi-docker-server:$SHA ./server/Dockerfile ./server
+docker build -t clauke/k8s-multi-docker-worker:latest -t clauke/k8s-multi-docker-worker:$SHA ./worker/Dockerfile ./worker
+
+# Push latest Images to Docker Hub
+docker push clauke/k8s-multi-docker-client:latest
+docker push clauke/k8s-multi-docker-server:latest
+docker push cclauke/k8s-multi-docker-worker:latest
+
+# Push SHA Images to Docker Hub for Version controll
+docker push clauke/k8s-multi-docker-client:$SHA
+docker push clauke/k8s-multi-docker-server:$SHA
+docker push cclauke/k8s-multi-docker-worker:$SHA
+
+# Apply the K8S Config
+kubectl apply -f k8s
+
+# Force to always use newest Image even without changes in configuration file(s)
+kubectl set image deployments/server-deployment server=clauke/k8s-multi-docker-client:$SHA
+kubectl set image deployments/server-deployment server=clauke/k8s-multi-docker-client:$SHA
+kubectl set image deployments/server-deployment server=clauke/k8s-multi-docker-client:$SHA
